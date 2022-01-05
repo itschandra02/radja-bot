da9c2562ce8b1ea6b893d1a4389a0d8f8d34ebaa
Radja Aditya Chandra <40448508+itschandra02@users.noreply.github.com>
2022-01-05 17:24:32 +0700

    Add files via upload

--- messageHandler.js
+++ messageHandler.js
@@ -1,4 +1,4 @@
-// https://github.com/itschandra02/radja-bot
+// https://github.com/salismazaya/whatsapp-bot
 
 const fs = require("fs");
 const axios = require("axios");
@@ -177,8 +177,9 @@ const text =
 		}
 		
 		case "!donasi":
-        case "!donate":
-{
+        case "!donate":       
+        {
+image = fs.readFileSync("lib/donasi.jpg", "utf-8")
 const text =`Hallo, kak bantu donasi nya dong 👋
 ╭──❉ *DONASI KAK* ❉─────
 │➸ *DANA*: 081262163214
